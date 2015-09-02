e6297c25d49572eb35dc1ed047841163832da192
oleg <traeop@gmail.com>
2015-09-03 01:22:33 +0300

    format code

--- js/canvas/settings/script.js
+++ js/canvas/settings/script.js
@@ -1,61 +1,60 @@
-
 ViewKonva.prototype.Settings = function(object) {
-	var _this = this;	
+    var _this = this;
 
-	_this.Data 								 = object.data;
+    _this.Data = object.data;
 
-	_this.Layer 							 = object.layer;
-	_this.Group 							 = object.group;
+    _this.Layer = object.layer;
+    _this.Group = object.group;
 
-	_this.ColorOtherCircles 	 = object.colorOtherCircles;
-	_this.NumberOtherCircles   = object.numberOtherCircles;
-	_this.BorderLineOther			 = object.borderLineOther;
-	_this.RadiusOtherCircle    = object.radiusOtherCircle;
-	_this.BorderLineContent		 = object.borderLineContent;
+    _this.ColorOtherCircles = object.colorOtherCircles;
+    _this.NumberOtherCircles = object.numberOtherCircles;
+    _this.BorderLineOther = object.borderLineOther;
+    _this.RadiusOtherCircle = object.radiusOtherCircle;
+    _this.BorderLineContent = object.borderLineContent;
 
-	_this.BorderLineItemMenu	 = object.borderLineItemMenu;
-	_this.ColorItemMenu 			 = object.colorItemMenu;
-	_this.RadiusItemMenu 			 = object.radiusItemMenu;
-	_this.ShadowActiveItemMenu = object.shadowActiveItemMenu;
-	_this.SpeedAllCircles 		 = object.speedAllCircles;
+    _this.BorderLineItemMenu = object.borderLineItemMenu;
+    _this.ColorItemMenu = object.colorItemMenu;
+    _this.RadiusItemMenu = object.radiusItemMenu;
+    _this.ShadowActiveItemMenu = object.shadowActiveItemMenu;
+    _this.SpeedAllCircles = object.speedAllCircles;
 
-	_this.FontSize 						 = object.fontSize;
-	_this.FontFamily  				 = object.fontFamily;
-	_this.ShadowColorText			 = object.shadowColorText;
+    _this.FontSize = object.fontSize;
+    _this.FontFamily = object.fontFamily;
+    _this.ShadowColorText = object.shadowColorText;
 
-	_this.SpeedLineItemMenu		 = object.speedLineItemMenu;
+    _this.SpeedLineItemMenu = object.speedLineItemMenu;
 
-	_this.BorderPreviewMenu    = object.borderPreviewMenu;
-	_this.ColorPreviewMenu		 = object.colorPreviewMenu;
-	_this.WHpreviewItemMenu    = object.whPreviewItemMenu;
+    _this.BorderPreviewMenu = object.borderPreviewMenu;
+    _this.ColorPreviewMenu = object.colorPreviewMenu;
+    _this.WHpreviewItemMenu = object.whPreviewItemMenu;
 
-	_this.FlagHoverItemMenu    = object.flagHoverItemMenu;
+    _this.FlagHoverItemMenu = object.flagHoverItemMenu;
 
-	_this.RadiusCirclesContent = object.radiusCirclesContent;
-	_this.HoverTextAnimate		 = object.hoverTextAnimate;
+    _this.RadiusCirclesContent = object.radiusCirclesContent;
+    _this.HoverTextAnimate = object.hoverTextAnimate;
 
-	_this.IdClose							 = object.idClose;
-	_this.AddClass						 = object.addClass;
+    _this.IdClose = object.idClose;
+    _this.AddClass = object.addClass;
 
-	
 
-	_this.CircleItemsMenu()
-		.OtherCircles()
-		.OtherLines()
 
-		.PreviewProjects()
-		.PreviewContent()
+    _this.CircleItemsMenu()
+        .OtherCircles()
+        .OtherLines()
 
-		.LineItemsMenu()
-		.LineTextItemMenu()
-		.TextItemMenu()
-		.ShowLineItemMenu()
-		.ShowPreviewItemMenu()
+    .PreviewProjects()
+        .PreviewContent()
 
-		.TextContent()
+    .LineItemsMenu()
+        .LineTextItemMenu()
+        .TextItemMenu()
+        .ShowLineItemMenu()
+        .ShowPreviewItemMenu()
 
-		.ConnectingLine()
+    .TextContent()
 
-		.ButtonClose();
+    .ConnectingLine()
+
+    .ButtonClose();
 
 };
