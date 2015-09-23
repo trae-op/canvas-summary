0e529ada52dd90888b61c96bd3e1edda97d6b101
oleg <traeop@gmail.com>
2015-09-23 12:13:57 +0300

    change view

--- js/canvas/view.js
+++ js/canvas/view.js
@@ -11,56 +11,59 @@ App.Views.MyView = new ViewKonva(function(_this) {
     rubberStage: true,
     initialize: function(stage, group, layer) {
 
-    if (_this.CheckMobile) {
+
+    _this.MobileFalse(function() {
+
+
+        _this.Settings({
+            layer: layer,
+            group: group,
+            data: App.Models.Data,
+            
+            speedAllCircles: 1,
+            
+            colorItemMenu: '#fff',
+            borderLineItemMenu: 1,
+            backgroundActiveItemMenu: '#888',
+          borderActiveItemMenu: 2,
+            radiusItemMenu: 15,
+
+            fontSize: 14,
+            fontFamily: 'Courier New',
+            shadowColorText: '#333',
+
+            speedLineItemMenu: 0.1,
+            borderPreviewMenu: 4,
+            colorPreviewMenu: '#fff',
+            whPreviewItemMenu: 35,
+            flagHoverItemMenu: false,
+            
+            numberOtherCircles: 40,
+            colorOtherCircles: '#777',
+            radiusOtherCircle: 5,
+            borderLineOther: 0.3,
+
+            borderLineContent: 3,
+            radiusCirclesContent: 27,
+            hoverTextAnimate: 0.3,
+          borderProjects: '#ccc', 
+
+          idClose: 'btn-close',
+          addClass: 'open-close'
+        });
+
+
+        stage.add( 
+            layer('layerLines'),
+            layer('layerMain'),
+            layer('layerText') 
+        );
+
+    }).MobileTrue(function() {
         document.getElementById('for-mobile')
             .classList
                 .remove('close-mobile');
-        return;
-    }
-
-    _this.Settings({
-    	layer: layer,
-    	group: group,
-    	data: App.Models.Data,
-    	
-    	speedAllCircles: 1,
-    	
-    	colorItemMenu: '#fff',
-    	borderLineItemMenu: 1,
-    	backgroundActiveItemMenu: '#888',
-      borderActiveItemMenu: 2,
-    	radiusItemMenu: 15,
-
-    	fontSize: 14,
-    	fontFamily: 'Courier New',
-    	shadowColorText: '#333',
-
-    	speedLineItemMenu: 0.1,
-    	borderPreviewMenu: 4,
-    	colorPreviewMenu: '#fff',
-    	whPreviewItemMenu: 35,
-    	flagHoverItemMenu: false,
-    	
-    	numberOtherCircles: 40,
-    	colorOtherCircles: '#777',
-    	radiusOtherCircle: 5,
-    	borderLineOther: 0.3,
-
-    	borderLineContent: 3,
-    	radiusCirclesContent: 27,
-    	hoverTextAnimate: 0.3,
-      borderProjects: '#ccc', 
-
-      idClose: 'btn-close',
-      addClass: 'open-close'
     });
-
-
-    stage.add( 
-        layer('layerLines'),
-        layer('layerMain'),
-        layer('layerText') 
-    );
 
     }
   };
